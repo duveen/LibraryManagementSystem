c3879836386395d2cf9e5e545bbdc8db26c33651
Duveen <duveen@duveen.me>
2016-04-16 15:35:17 +0900

    책 검색기능 추가

---
+++ src/kr/o3selab/library/gui/child/SearchBook.java
@@ -0,0 +1,16 @@
+package kr.o3selab.library.gui.child;
+
+import javax.swing.JInternalFrame;
+
+public class SearchBook extends JInternalFrame {
+	public SearchBook() {
+		setBounds(0, 0, 420, 300);
+		setTitle("µî·Ï");
+		setResizable(false);
+		setMaximizable(false);
+		setClosable(true);
+		
+		setVisible(true);
+	}
+	
+}
